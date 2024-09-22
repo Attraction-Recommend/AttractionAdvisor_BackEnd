107550617f772a73f16a3709191aa32aef7fe037
plan11plan <tjtfy7028@gmail.com>
2024-09-22 14:52:11 +0900

    first commit

---
+++ src/main/java/com/example/attractionadvisor_backend/AttractionAdvisorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.attractionadvisor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttractionAdvisorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttractionAdvisorBackendApplication.class, args);
+    }
+
+}
